805ecaf4b16fc637962887836564e5fc9705675a
yangnan <18834196814@163.com>
2019-07-01 13:36:13 +0800

    修改线上数据库连接问题

--- web/src/main/java/com/yangnan/blog/WebApplication.java
+++ web/src/main/java/com/yangnan/blog/WebApplication.java
@@ -3,6 +3,8 @@ package com.yangnan.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,8 +14,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(scanBasePackages = "com.yangnan.blog")
 @EnableAspectJAutoProxy(exposeProxy = true)
 @EnableScheduling
-public class WebApplication {
-
+public class WebApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(WebApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
